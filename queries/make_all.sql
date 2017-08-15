dbc332c3172304a5f3a9d2aebe5bb020971442e2
Alistair Johnson <alistairewj@gmail.com>
2017-08-15 19:17:37 -0400

    init query to make all tables

---
+++ queries/make_all.sql
@@ -0,0 +1,20 @@
+-- tables required before generating the data matters tables
+
+-- for cohort
+mp_code_status
+mp_bg_art
+
+-- for data
+mp_data
+mp_static_data
+
+
+\i dm_cohort.sql
+\i dm_word_count.sql
+\i dm_static_data.sql
+\i dm_service.sql
+\i dm_obs_count.sql
+\i dm_number_of_notes.sql
+\i dm_intime_outtime.sql
+\i dm_dialysis_start.sql
+\i dm_braindeath.sql
